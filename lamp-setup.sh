ee792aa2983febfc2abb6b5a3817234b5a66b16c
Rodrigo Manara <rodrigomanara@yahoo.co.uk>
2016-12-20 19:08:31 +0000

    Update and rename install.sh to lamp-setup.sh

--- lamp-setup.sh
+++ lamp-setup.sh
@@ -3,9 +3,12 @@
 
 sudo apt-get install apache2
 sudo apt-get install libapache2-mod-php5
+sudo apt-get install php ## php7
 
+sudo apt-get install mysql-server
+##install symfony
 sudo curl -LsS https://symfony.com/installer -o /usr/local/bin/symfony
 sudo chmod a+x /usr/local/bin/symfony
-
-sudo apt-get install curl php5-cli git
+## install curl , git
+sudo apt-get install curl git
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
