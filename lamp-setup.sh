06fe874fd3480d31c053a189945147877f62cc59
Rodrigo Manara <rodrigomanara@yahoo.co.uk>
2016-12-20 19:12:04 +0000

    Update lamp-setup.sh

--- lamp-setup.sh
+++ lamp-setup.sh
@@ -1,13 +1,14 @@
-## Setup Linux and Apache
+## Setup Linux web Developement using Apache
 ### command line
-
 sudo apt-get install apache2
+##php
 sudo apt-get install php ## php7
-
+##mysql
 sudo apt-get install mysql-server
+## install curl , git
+sudo apt-get install curl git
 ##install symfony
 sudo curl -LsS https://symfony.com/installer -o /usr/local/bin/symfony
 sudo chmod a+x /usr/local/bin/symfony
-## install curl , git
-sudo apt-get install curl git
+
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
